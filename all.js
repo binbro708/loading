c5a24a478e52dfc2003179667814a4f01340c94e
B502-16\user <wboirnk@gmail.com>
2022-10-14 11:32:25 +0800

    first commit

---
+++ all.js
@@ -0,0 +1,6 @@
+gsap.registerPlugin(ScrollTrigger, TextPlugin);
+
+gsap.to('.textA', {opacity:1, duration:10})
+gsap.to('.textB', {opacity:1, duration:15})
+gsap.to('.textC', {opacity:1, duration:20})
+gsap.to('.textD', {opacity:1, duration:25})
